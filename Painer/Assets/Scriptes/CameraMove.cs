15accc07b5d5cce1cf8e2ae7d11b0e3f8a9cf043
hinaheadisnotbig <kko20_s23_10102@gclass.ice.go.kr>
2024-06-18 21:14:34 +0900

    feat:CameraMode

--- Painer/Assets/Scriptes/CameraMove.cs
+++ Painer/Assets/Scriptes/CameraMove.cs
@@ -1,19 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
+
 
 public class CameraMove : MonoBehaviour
 {
+    public int cameraMode = 0;
+    public GameObject BG;
+    public GameObject BG2;
     public GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ChangeCameraMode(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Player.transform.position + new Vector3(10,1,0);
+
+        if (cameraMode == 0)
+        {
+            BG.SetActive(true);
+            BG2.SetActive(false);
+            transform.position = Player.transform.GetChild(0).transform.position + new Vector3(9, 0, 0);
+        }
+        if (cameraMode == 1)
+        {
+            BG.SetActive(false);
+            BG2.SetActive(true);
+            transform.position = Player.transform.GetChild(0).transform.position + new Vector3(0, 2, -4);
+        }
+        transform.LookAt(Player.transform.GetChild(0).transform, Vector3.up);
+           
+        
+    }
+
+     public void ChangeCameraMode(int mode)
+    {
+        cameraMode = mode;
+        Player.GetComponent<PlayerSystem>().MovePlayer(mode);
     }
 }
--- Painer/Assets/Scriptes/PlayerSystem.cs
+++ Painer/Assets/Scriptes/PlayerSystem.cs
@@ -1,18 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public CameraMove cam;
+    public float speed = 5f;
+    public bool isjumping = false;
+    private Rigidbody characterRigidbody;
+    public float jumpPower = 7;
+    Vector3 dir = Vector3.zero;
+
     void Start()
     {
-        
+        characterRigidbody = GetComponent<Rigidbody>();
+    }
+    void inputAndDir()
+    {
+        if (cam.cameraMode == 0)
+        {
+        dir.z = Input.GetAxis("Vertical");   // x축 방향 키 입력
+        dir.x = 0;     // z축 방향 키 입력
+        }
+          if (cam.cameraMode == 1)
+        {
+        dir.x = Input.GetAxis("Horizontal");   // x축 방향 키 입력
+        dir.z = Input.GetAxis("Vertical");     // z축 방향 키 입력
+        }
+        if (dir != Vector3.zero)   // 키입력이 존재하는 경우
+        {
+            transform.forward = dir;	// 키 입력 시, 입력된 방향으로 캐릭터의 방향을 바꿈
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space) && !isjumping)
+        {
+            isjumping = true;
+            characterRigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+        inputAndDir();
     }
+    void FixedUpdate()
+    {
+        characterRigidbody.MovePosition(characterRigidbody.position + dir * speed * Time.deltaTime);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        UnityEngine.Debug.Log("닿음");
+        if(collision.transform.tag == "Ground")
+        {
+            isjumping = false;
+            UnityEngine.Debug.Log("1");
+        }
+    }
+
+    public void MovePlayer(int mode)
+    {
+        if(mode == 0)
+        {
+            transform.position = new Vector3(4.93f, transform.position.y, transform.position.z);
+        }
+        if (mode == 1)
+        {
+            transform.position = new Vector3(2.85f, transform.position.y, transform.position.z);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.CompareTag("mode1"))
+        {
+            cam.ChangeCameraMode(0);
+            Destroy(other);
+        }
+        if (other.transform.CompareTag("mode2"))
+        {
+            cam.ChangeCameraMode(1);
+            Destroy(other);
+        }
+    }
+
 }
+
